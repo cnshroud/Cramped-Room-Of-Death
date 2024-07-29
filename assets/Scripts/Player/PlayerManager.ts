0163d1307c690c59a4dd25cf15cae95fc93eca40
xyx <xyx123>
2024-07-29 11:15:40 +0800

    实现player人物移动

--- assets/Scripts/Player/PlayerManager.ts
+++ assets/Scripts/Player/PlayerManager.ts
@@ -2,15 +2,70 @@
 import { _decorator, animation, Animation, AnimationClip, Component, Node, Sprite, SpriteFrame, UITransform, Vec3 } from 'cc';
 import { TILE_HEIGHT, TILE_WIDTH } from '../Tile/TileManager';
 import { ResourceManager } from '../../Runtime/ResourceManager';
+import { CONTORLLER_ENUM, EVENT_ENUM } from '../../Enum';
+import { EventManager } from '../../Runtime/EventManager';
 const { ccclass, property } = _decorator;
 
 //帧数常量 1秒8帧
 const ANIMATION_SPEED=1/8
+
+//玩家管理器，管理玩家动画
 @ccclass('PlayerManager')
 export class PlayerManager extends Component {
+    //角色移动思路，一共两个坐标，角色的坐标x,y,和角色的目标坐标targetX,targetY
+    //监听键盘事件修改目的坐标，在update中不断让角色坐标靠近目的坐标
+    x:number=0
+    y:number=0
+    targetX:number=0
+    targetY:number=0
+    private readonly speed:number=1/10
+
+    update(){
+        this.updateXY()
+        //设置角色位置，因为y是相反的所以要给-，人物宽度是四个瓦片的宽度，所以要把人物的坐标移动固定位置
+        this.node.setPosition(this.x*TILE_WIDTH-TILE_WIDTH*1.5,-this.y*TILE_HEIGHT+TILE_HEIGHT*1.5)
+    }
 
 
     async init(){
+        await this.render()
+        //把move方法绑定到evenetmanegr
+        EventManager.Instance.on(EVENT_ENUM.PLAYER_CTRL,this.move,this)
+    };
+
+    updateXY(){
+        if(this.targetX<this.x){
+            this.x-=this.speed
+        }else if(this.targetX>this.x){
+            this.x+=this.speed
+        }
+        if(this.targetY<this.y){
+            this.y-=this.speed
+        }else if(this.targetY>this.y){
+            this.y+=this.speed
+        }
+        //当角色坐标和目标坐标差距小于0.1时，认为角色已经到达目标坐标，此时将角色坐标设置为和目标坐标相同
+        if(Math.abs(this.targetX-this.x)<=0.1 &&Math.abs(this.targetY-this.y)<=0.1){
+            this.x=this.targetX
+            this.y=this.targetY
+        }
+    }
+
+    //人物移动
+    move(inputDirction:CONTORLLER_ENUM){
+        if(inputDirction==CONTORLLER_ENUM.TOP){
+            this.targetY-=1
+        }else if(inputDirction==CONTORLLER_ENUM.BOTTOM){
+            this.targetY+=1
+        }else if(inputDirction==CONTORLLER_ENUM.LEFT){
+            this.targetX-=1
+        }else if(inputDirction==CONTORLLER_ENUM.RIGHT){
+            this.targetX+=1
+        }
+    }
+
+    //渲染玩家
+    async render(){
         //角色sprite
         const sprite = this.addComponent(Sprite)
         //自定义宽高
@@ -25,15 +80,16 @@ export class PlayerManager extends Component {
 
 
         //coocs官网的程序化编辑动画剪辑的例子
-
-
         const animationClip = new AnimationClip();
 
 
         const track  = new animation.ObjectTrack(); // 创建一个对象轨道
          // 指定轨道路径，即指定目标对象为 "Sprite" 子节点的 "spriteFrame" 属性
         track.path = new animation.TrackPath().toComponent(Sprite).toProperty('spriteFrame');
-        //关键帧列表,需要给个类型，数字是帧，类型是SpriteFrame
+        //关键帧列表,需要给个类型，数字是帧，类型是SpriteFrame,
+        //map方法会遍历数组中的每个元素，并对每个元素执行提供的函数。
+        //回调函数中的参数顺序是固定的：第一个参数是当前元素，第二个参数是当前元素的索引（如果提供了第二个参数的话）
+
         const frames:Array<[number,SpriteFrame]> = spriteFrames.map((item,index)=>[ANIMATION_SPEED*index,item])
 
         track.channel.curve.assignSorted(frames);
@@ -49,5 +105,5 @@ export class PlayerManager extends Component {
         animationComponent.defaultClip = animationClip;
         //播放组件
         animationComponent.play()
-    };
+    }
 }
--- assets/Scripts/UI/ControllerManager.ts
+++ assets/Scripts/UI/ControllerManager.ts
@@ -1,7 +1,7 @@
 
 import { _decorator, Component, Node } from 'cc';
 import { EventManager } from '../../Runtime/EventManager';
-import { EVENT_ENUM } from '../../Enum';
+import { CONTORLLER_ENUM, EVENT_ENUM } from '../../Enum';
 const { ccclass, property } = _decorator;
 /**
  *
@@ -10,7 +10,10 @@ const { ccclass, property } = _decorator;
 @ccclass('ControllerManager')
 export class ControllerManager extends Component {
     //执行事件中心的方法
-    handleCtrl(){
-        EventManager.Instance.emit(EVENT_ENUM.NEXT_LEVEL)
+    handleCtrl(evt:Event,type:string){
+        // EventManager.Instance.emit(EVENT_ENUM.NEXT_LEVEL)
+        //将button事件的返回字符拿到，强转为CONTORLLER_ENUM
+        EventManager.Instance.emit(EVENT_ENUM.PLAYER_CTRL,type as CONTORLLER_ENUM)
+
     }
 }
